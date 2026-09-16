906792c0c590745f6768508c08c5a12b5f8524a8
michal-plechawski <michal@macbook-air-paula.home>
2026-09-16 14:02:02 +0200

    Fix trace metadata reader detection for clang

--- src/Altirra/source/tracefileformat.cpp
+++ src/Altirra/source/tracefileformat.cpp
@@ -75,7 +75,7 @@ void ATSavedTraceFrameChannelDetail::FrameBoundaryInfo::Exchange(T_Self& self, T
 	rw.Transfer("period", &self.mPeriod);
 	rw.Transfer("count", &self.mCount);
 
-	if constexpr(rw.IsReader) {
+	if constexpr(T_Ex::IsReader) {
 		if (self.mCount == 0 || self.mPeriod == 0)
 			throw ATInvalidSaveStateException();
 	}
@@ -86,7 +86,7 @@ void ATSavedTraceFrameChannelDetail::Exchange(T& rw) {
 	rw.Transfer("ticks_per_second", &mTicksPerSecond);
 	rw.Transfer("frame_boundaries", &mFrameBoundaries);
 
-	if constexpr(rw.IsReader) { 
+	if constexpr(T::IsReader) {
 		if (mTicksPerSecond < 1770000.0 || mTicksPerSecond > 1790000.0)
 			throw ATUnsupportedSaveStateException();
 	}
@@ -115,7 +115,7 @@ void ATSavedTraceCPUChannelDetail::Exchange(T& rw) {
 	rw.Transfer("byte_count", &mRowSize);
 	rw.Transfer("blocks", &mBlocks);
 
-	if constexpr(rw.IsReader) {
+	if constexpr(T::IsReader) {
 		// stripe must be at least one byte wide
 		if (mRowSize == 0)
 			throw ATInvalidSaveStateException();
@@ -189,7 +189,7 @@ void ATSavedTraceVideoChannelDetail::Exchange(T& rw) {
 	rw.Transfer("frame_buffer_info", &mpFrameBufferInfo);
 	rw.Transfer("frame_buffer_groups", &mFrameBufferGroups);
 
-	if constexpr(rw.IsReader) {
+	if constexpr(T::IsReader) {
 		if (!mFrameBuffersPerGroup || mFrameBuffersPerGroup > 65536)
 			throw ATInvalidSaveStateException();
 	}
