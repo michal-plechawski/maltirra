90c9c90925deb776acf07a10aa87f96efc5463f7
michal-plechawski <michal@macbook-air-paula.home>
2026-09-17 09:08:05 +0200

    Fix R-Time 8 serialization portability

--- src/Altirra/source/rtime8.cpp
+++ src/Altirra/source/rtime8.cpp
@@ -155,7 +155,7 @@ void ATSaveStateRTime8::Exchange(T& ex) {
 	ex.Transfer("address", &mAddress);
 	ex.Transfer("phase", &mPhase);
 
-	if constexpr (ex.IsReader) {
+	if constexpr (T::IsReader) {
 		mAddress &= 0x0f;
 
 		if (mPhase >= 3)
